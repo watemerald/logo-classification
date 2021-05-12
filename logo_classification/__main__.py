64af958ef13aeff699a02495fc61a56e952bb25f
Andrii Sherman <andriisherman@gmail.com>
2021-05-11 23:43:31 -0400

    Train baseline model

--- logo_classification/__main__.py
+++ logo_classification/__main__.py
@@ -1,4 +1,6 @@
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
 
 from .config import settings
 from .data import Logo2kDataModule
@@ -9,8 +11,18 @@ def main():
     dm = Logo2kDataModule(settings.DATASET_FOLDER)
     model = Logo2kTransferModel(dm.num_classes)
 
-    trainer = pl.Trainer(gpus=-1, max_epochs=1)
+    wandb_logger = WandbLogger(project="logo-classification")
+    tensorboard_logger = TensorBoardLogger("lightning_logs")
+
+    trainer = pl.Trainer(
+        gpus=-1,
+        max_epochs=10,
+        logger=[wandb_logger, tensorboard_logger],
+        callbacks=[EarlyStopping(monitor="val_acc", patience=2)],
+    )
     trainer.fit(model, dm)
+
+    trainer.test(model, datamodule=dm)
 
 
 if __name__ == "__main__":
--- logo_classification/model.py
+++ logo_classification/model.py
@@ -1,5 +1,6 @@
 import pytorch_lightning as pl
 import torch.nn.functional as F
+import torchmetrics
 from torch import nn, optim
 from torchvision import models
 
@@ -11,6 +12,10 @@ class Logo2kTransferModel(pl.LightningModule):
         self.save_hyperparameters()
         self.output_classes = output_classes
         self.lr = lr
+
+        self.train_acc = torchmetrics.Accuracy()
+        self.validation_acc = torchmetrics.Accuracy()
+        self.test_acc = torchmetrics.Accuracy()
 
         # Load pretrained model (except for final layer), and freeze it
         backbone = models.resnet50(pretrained=True)
@@ -33,28 +38,64 @@ class Logo2kTransferModel(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        logits = self(x)
-        loss = F.nll_loss(logits, y)
+        preds = self(x)
+        loss = F.nll_loss(preds, y)
 
-        self.log("train_loss", loss, on_step=True, on_epoch=True, logger=True)
+        self.train_acc(preds, y)
+
+        self.log(
+            "train_loss", loss, on_step=True, on_epoch=True, logger=True, prog_bar=True
+        )
+        self.log(
+            "train_acc",
+            self.train_acc,
+            on_step=True,
+            on_epoch=True,
+            logger=True,
+            prog_bar=True,
+        )
 
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
-        logits = self(x)
-        loss = F.nll_loss(logits, y)
+        preds = self(x)
+        loss = F.nll_loss(preds, y)
 
-        self.log("val_loss", loss, on_step=True, on_epoch=True, logger=True)
+        self.validation_acc(preds, y)
+
+        self.log(
+            "val_loss", loss, on_step=True, on_epoch=True, logger=True, prog_bar=True
+        )
+        self.log(
+            "val_acc",
+            self.validation_acc,
+            on_step=True,
+            on_epoch=True,
+            logger=True,
+            prog_bar=True,
+        )
 
         return loss
 
     def test_step(self, batch, batch_idx):
         x, y = batch
-        logits = self(x)
-        loss = F.nll_loss(logits, y)
+        preds = self(x)
+        loss = F.nll_loss(preds, y)
 
-        self.log("test_loss", loss, on_step=True, on_epoch=True, logger=True)
+        self.test_acc(preds, y)
+
+        self.log(
+            "test_loss", loss, on_step=True, on_epoch=True, logger=True, prog_bar=True
+        )
+        self.log(
+            "test_acc",
+            self.test_acc,
+            on_step=True,
+            on_epoch=True,
+            logger=True,
+            prog_bar=True,
+        )
 
         return loss
 
